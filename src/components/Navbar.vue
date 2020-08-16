9fce0bed6e6ca8af5cbf08fe89c3eebfb20b73c5
Boyko03 <boyko100@abv.bg>
2020-08-16 20:31:01 +0300

    Added a navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand to="/">NavBar</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="/">Home</b-nav-item>
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto" v-show="false">
+          <b-nav-item-dropdown text="Lang" right>
+            <b-dropdown-item href="#">EN</b-dropdown-item>
+            <b-dropdown-item href="#">ES</b-dropdown-item>
+            <b-dropdown-item href="#">RU</b-dropdown-item>
+            <b-dropdown-item href="#">FA</b-dropdown-item>
+          </b-nav-item-dropdown>
+
+          <b-nav-item-dropdown right>
+            <!-- Using 'button-content' slot -->
+            <template v-slot:button-content>
+              <em>User</em>
+            </template>
+            <b-dropdown-item href="#">Profile</b-dropdown-item>
+            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+        <b-navbar-nav class="ml-auto" v-show="true">
+          <b-nav-item-dropdown text="Lang" right>
+            <b-dropdown-item href="#">EN</b-dropdown-item>
+            <b-dropdown-item href="#">ES</b-dropdown-item>
+            <b-dropdown-item href="#">RU</b-dropdown-item>
+            <b-dropdown-item href="#">FA</b-dropdown-item>
+          </b-nav-item-dropdown>
+
+          <b-nav-item to="/register">Sign Up</b-nav-item>
+          <b-nav-item to="login">Sign In</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+  import 
+  {
+    BNavbar,
+    BNavbarNav,
+    BNavbarBrand,
+    BNavbarToggle,
+    BCollapse,
+    BNavItem,
+    BNavItemDropdown,
+    BDropdownItem
+  } from 'bootstrap-vue'
+
+  export default {
+    name: 'Navbar',
+    components: {
+      BNavbar,
+      BNavbarNav,
+      BNavbarBrand,
+      BNavbarToggle,
+      BCollapse,
+      BNavItem,
+      BNavItemDropdown,
+      BDropdownItem
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
