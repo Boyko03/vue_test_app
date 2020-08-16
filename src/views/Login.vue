580f7eda1d7c7280b9ac77f7def6d33a1dc18760
Boyko03 <boyko100@abv.bg>
2020-08-16 20:30:38 +0300

    Added login and register page

---
+++ src/views/Login.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Navbar/>
+    Login
+  </div>
+</template>
+
+<script>
+  import Navbar from '@/components/Navbar.vue'
+
+  export default {
+    name: "Login",
+    components: {
+      Navbar,
+    },
+  };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/Register.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Navbar/>
+    Register
+  </div>
+</template>
+
+<script>
+  import Navbar from '@/components/Navbar';
+
+  export default {
+    name: 'Register',
+    components: {
+      Navbar,
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
