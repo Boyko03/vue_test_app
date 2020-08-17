4b03152288d5652830e6c5620a50e26c0e26bc92
Boyko03 <boyko100@abv.bg>
2020-08-17 21:39:54 +0300

    Added login form

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,21 +1,102 @@
 <template>
   <div>
-    <Navbar/>
-    Login
+    <div class="d-flex justify-content-center">
+      <b-card
+        class="my-3"
+        header="Sign In"
+        border-variant="secondary"
+        header-border-variant="secondary"
+      >
+        <b-form id="myForm" @submit="onSubmit" v-if="show" :validated="valid" :novalidate="true">
+          <b-form-group
+            id="email-input-group"
+            label="Email address:"
+            label-for="email"
+          >
+            <b-form-input
+              id="email"
+              v-model="form.email"
+              type="email"
+              required
+              placeholder="Enter email"
+            ></b-form-input>
+          </b-form-group>
+
+          <b-form-group id="password-input-group" label="Password:" label-for="password">
+            <b-form-input
+              type="password"
+              id="password"
+              v-model="form.password"
+              placeholder="Enter password"
+              required
+              minlength="8"
+              maxlength="20"
+            ></b-form-input>
+          </b-form-group>
+
+          <b-form-group>
+            <b-button type="submit" variant="primary">Sign In</b-button>
+            <label aria-describedby="not-have-an-account-help-block" />
+            <b-form-text id="not-have-an-account-help-block">
+              <b-link to="/register" class="card-link">Don't have an account? Sign Up now!</b-link>
+            </b-form-text>
+          </b-form-group>
+        </b-form>
+      </b-card>
+    </div>
+    <!--
+    <b-card class="mt-3" header="Form Data Result">
+      <pre class="m-0">{{ form }}</pre>
+    </b-card>
+    -->
   </div>
 </template>
 
 <script>
-  import Navbar from '@/components/Navbar.vue'
-
+  import {
+    BForm,
+    BFormGroup,
+    BFormInput,
+    BFormText,
+    BButton,
+    BCard,
+    BLink,
+  } from 'bootstrap-vue';
+ 
   export default {
-    name: "Login",
+    name: 'Register',
     components: {
-      Navbar,
+      BForm,
+      BFormGroup,
+      BFormInput,
+      BFormText,
+      BButton,
+      BCard,
+      BLink,
     },
-  };
+    data() {
+      return {
+        form: {
+          email: '',
+          password: '',
+        },
+        show: true,
+        valid: false,
+      }
+    },
+    methods: {
+      onSubmit(evt) {
+        this.valid = true
+
+        var form = document.getElementById('myForm')
+        if (form.checkValidity() === false) {
+          evt.preventDefault();
+          evt.stopPropagation();
+        }
+        else {
+          alert(JSON.stringify(this.form))
+        }
+      },
+    }
+  }
 </script>
-
-<style lang="scss" scoped>
-
-</style>
